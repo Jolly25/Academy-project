8621a17ccb90a1fdbcd3c7cc5aba8560122ced62
gabriele.marchetti <gabriele.marchetti@linksmt.it>
2023-10-30 11:09:03 +0100

    now it works

--- AcademyBackend/src/test/java/com/corso/test/CheckStringTest.java
+++ AcademyBackend/src/test/java/com/corso/test/CheckStringTest.java
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.corso.beans.Beans;
 import com.corso.checkstring.AlgorithmHandler;
 import com.corso.checkstring.CheckString;
-import com.corso.dao.MatchDAO;
-import com.corso.model.Match;
-import com.corso.standardwords.AlgorithmType;
+
 
 public class CheckStringTest {
 
 	public static void main(String[] args) {
-		
+
 		AnnotationConfigApplicationContext factory = new AnnotationConfigApplicationContext(Beans.class);
 
 		CheckString cs = (CheckString) factory.getBean("c1"); 
@@ -20,17 +18,10 @@ public class CheckStringTest {
 		
 		ah.buildCheckString();
 		
-		String s = "Germanyy";
-		cs.check(s.toUpperCase()); //ho messo toUpperCase perchè nel mio db le parole standard sono in stampatello
+		cs.check("Germanyy");
 		
-		/*
-		MatchDAO dao = factory.getBean("matchDao", MatchDAO.class);
-	 	Match m = new Match();
-	 	m.setInput("Francee");
-	 	m.setStandardword("FRANCE");
-	 	m.setAlgorithm(AlgorithmType.Admin);
-	 	dao.create(m);
-		*/
+		
+		
 	}
 
 }
