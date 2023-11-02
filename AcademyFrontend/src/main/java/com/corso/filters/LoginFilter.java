58045dcbe1095610eab75b4a25bf7ed52588db37
gabriele.marchetti <gabriele.marchetti@linksmt.it>
2023-11-02 12:09:37 +0100

    fix logout

--- AcademyFrontend/src/main/java/com/corso/filters/LoginFilter.java
+++ AcademyFrontend/src/main/java/com/corso/filters/LoginFilter.java
@@ -16,19 +16,25 @@ public class LoginFilter implements Filter{
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpServletResponse res = (HttpServletResponse) response;
+
+		//prevent browser to load page from cache when click back arrow
+		res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		res.setDateHeader("Expires", 0); // Proxies.
+
 		HttpSession s = req.getSession();
-		
+
 		System.out.println("filter");
-		
+
 		if(s.getAttribute("user") == null) {
 			res.setStatus(403);
 			return;
 		}
 		chain.doFilter(request, response);
-		
+
 	}
 
 }
