6833e17676e354844ae2933d5fb4d44235102092
gabriele.garavelli <gabriele.garavelli@dechit.it>
2023-11-08 10:31:57 +0100

    flag randomized

--- AcademyBackend/src/main/java/com/corso/service/StandardWordService.java
+++ AcademyBackend/src/main/java/com/corso/service/StandardWordService.java
@@ -1,5 +1,6 @@
 package com.corso.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.corso.dao.StandardWordDAO;
@@ -11,6 +12,17 @@ public class StandardWordService {
 	
 	public List<StandardWord> findAll() {
 		return dao.findAllSW();
+	}
+	
+	public List<String> getRandomStandardWords(int n) {
+		List<StandardWord> allsw = dao.findAllSW();
+		List<String> randomlist = new ArrayList<>();
+		while(randomlist.size() < n - 1) {
+			int randomNum = (int)(Math.random() * allsw.size());
+			String swtoadd = allsw.get(randomNum).getValue();
+			if(!randomlist.contains(swtoadd)) randomlist.add(swtoadd);
+		}
+		return randomlist;
 	}
 	
 	public void setDao(StandardWordDAO dao) {
