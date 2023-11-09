e5ee376e141dbf96991a82ee28b15350060c42f2
gabriele.marchetti <gabriele.marchetti@linksmt.it>
2023-11-09 15:49:01 +0100

    fix previous flag null

--- AcademyFrontend/src/main/webapp/resources/js/game.js
+++ AcademyFrontend/src/main/webapp/resources/js/game.js
@@ -190,20 +190,25 @@ function displayFlag(flagURL, countryName) {
 
 
 window.addEventListener('load', function() {
-	if(previousFlag != null || previousMatch != null || previousInput != null) {
-	console.log(previousMatch);
-	console.log(previousFlag);
-	if(previousMatch === null) {
-		previousMatch = "";
+	if(sessionStorage.getItem("firstLoad") === null) {
+		sessionStorage.setItem("firstLoad", true);
 	}
-	  if (previousMatch.toLowerCase().trim() === previousFlag.toLowerCase().trim()) {
-	    showCorrectModal(previousInput, previousMatch); // Visualizza la modale per la risposta corretta
-	    incrementCounter();
-	  } else {
-	    showWrongModal(previousInput, previousMatch, previousFlag); // Visualizza la modale per la risposta errata
-	  }
-	  
+	else {
+		if(previousFlag != null || previousMatch != null || previousInput != null) {
+			if(previousMatch === null) previousMatch = "";
+			
+			if(previousFlag == null) showWrongModal(previousInput, previousMatch, previousFlag);
+			else {
+				if (previousMatch.toLowerCase().trim() === previousFlag.toLowerCase().trim()) {
+				    showCorrectModal(previousInput, previousMatch); // Visualizza la modale per la risposta corretta
+				    incrementCounter();
+			  	} else {
+			    	showWrongModal(previousInput, previousMatch, previousFlag); // Visualizza la modale per la risposta errata
+			  	}
+			}
+		}
 	}
+
 });
 
 fetchData();
@@ -227,7 +232,8 @@ fetchData();
   function showWrongModal(previousInput, previousMatch, previousFlag) {
     const wrongModal = document.getElementById("wrongModal");
     const correctAnswerElement = document.getElementById("correctAnswer");
-    correctAnswerElement.textContent = "Hai inserito: " + previousInput + " = " + previousMatch + "\n Risposta corretta: " + previousFlag;
+    if(previousFlag === null) correctAnswerElement.textContent = "Nessun inserimento";
+    else correctAnswerElement.textContent = "Hai inserito: " + previousInput + " = " + previousMatch + "\n Risposta corretta: " + previousFlag;
     wrongModal.style.display = "block";
     setTimeout(() => {
       wrongModal.style.display = "none";
