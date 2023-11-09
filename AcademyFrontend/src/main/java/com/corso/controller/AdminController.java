7d44094a72652615da77454fb4c036a3a575622e
gabriele.garavelli <gabriele.garavelli@dechit.it>
2023-11-09 12:03:37 +0100

    ajax ahahah

--- AcademyFrontend/src/main/java/com/corso/controller/AdminController.java
+++ AcademyFrontend/src/main/java/com/corso/controller/AdminController.java
@@ -1,15 +1,20 @@
 package com.corso.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.corso.model.Match;
 import com.corso.service.MatchService;
+import com.google.gson.Gson;
 
 
 @Controller
@@ -23,6 +28,27 @@ public class AdminController {
 	public String getHomeAdmin() {
 		return "homeAdmin";
 	}
+	
+	@GetMapping("/matchlist")
+	public void getMatchList(HttpServletResponse response) {
+		System.out.println("We are in get");
+	    List<Match> matchList = matchService.getAllUncheckedMatches();
+	    String json = new Gson().toJson(matchList);
+	    try {
+		    response.setContentType("application/json");
+		    response.setCharacterEncoding("UTF-8");
+			response.getWriter().write(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@PostMapping("/matchlist")
+	public void postMatchList() {
+		System.out.println("We are in post");
+	}
+	
+	
 	
 	@GetMapping("/view_matchlist")
 	public String viewMatcheList(Model m) {
--- AcademyFrontend/src/main/webapp/WEB-INF/jsp/homeAdmin.jsp
+++ AcademyFrontend/src/main/webapp/WEB-INF/jsp/homeAdmin.jsp
@@ -5,11 +5,38 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Home Admin</title>
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <script>
+            $(document).on("click", "#buttonLoad", function() {
+                $.get("matchlist", function(responseJson) {
+                    var $select = $("#list");
+                    $select.find("option").remove();  
+                    $.each(responseJson, function(index, match) {
+                        $("<option>").val(match.id).text(match.input).appendTo($select);
+                    });                   
+                     
+                });
+            });
+             
+            $(document).on("click", "#buttonSubmit", function() {
+                var params = {category : $("#dropdownlist option:selected").text()};
+                $.post("matchlist", $.param(params), function(responseText) {
+                    alert(responseText);                   
+                });
+            });
+        </script>
+
 </head>
 <body>
 
 <a href="view_matchlist">Visualizza lista dei match</a> <br>
 <a href="logout">Logout</a>
-
+<div>
+<h2>Dynamic Drop Down List (AJAX) Demo</h2>
+    <button id="buttonLoad">Load</button> &nbsp;
+    <select id="list"></select>
+    <br/><br/>
+    <button id="buttonSubmit">Submit</button>
+</div>
 </body>
 </html>
