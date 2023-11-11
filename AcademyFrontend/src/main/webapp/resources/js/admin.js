117b8b24bf8943b05253053cde4440dcf11d39d1
gabriele.marchetti <gabriele.marchetti@linksmt.it>
2023-11-11 11:08:21 +0100

    pull fail

--- AcademyFrontend/src/main/webapp/resources/js/admin.js
+++ AcademyFrontend/src/main/webapp/resources/js/admin.js
@@ -13,16 +13,16 @@ var loadMatchList = function() {
         var $ul = $("#matchlist"); 
         $ul.find("li").remove();
         $.each(responseJson, function(index, match) {
-            var $li = $("<li>").val(match.id).text(match.input + "  =>  " + match.standardword).appendTo($ul);
+            var $li = $("<li>").text(match.input + "  =>  " + match.standardword).appendTo($ul);
             $li.addClass('list-group-item d-flex justify-content-between align-items-center match-item');
-            var $button1 = $("<button>").val(match.id + "ba").text("Accetta").appendTo($li);
+            var $button1 = $("<button>").text("Accetta").appendTo($li);
             $button1.addClass('btn btn-success btn-sm accept-button');
             $button1.click(function() {
 			   var param = {matchid : match.id};
                $.post("checkmatch",  $.param(param));
                $li.remove();
                });
-            var $button2 = $("<button>").val(match.id + "br").text("Elimina").appendTo($li);
+            var $button2 = $("<button>").text("Elimina").appendTo($li);
             $button2.addClass('btn btn-danger btn-sm reject-button');
             $button2.click(function() {
 			   var param = {matchid : match.id};
