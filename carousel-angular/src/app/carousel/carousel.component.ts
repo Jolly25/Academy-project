188d98a42f8870744858eac72acdbe41959a49d0
gabriele.marchetti <gabriele.marchetti@linksmt.it>
2023-11-20 12:14:57 +0100

    swap slides

--- carousel-angular/src/app/carousel/carousel.component.ts
+++ carousel-angular/src/app/carousel/carousel.component.ts
@@ -9,7 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent{
-  slides: string [] = ['./assets/slides/slide1.svg', './assets/slides/slide2.svg', './assets/slides/slide3.svg', './assets/slides/slide4.svg', './assets/slides/slide5.svg', './assets/slides/slide6.svg', './assets/slides/slide7.svg', './assets/slides/slide8.svg', './assets/slides/slide9.svg', './assets/slides/slide10.svg', './assets/slides/slide11.svg', './assets/slides/slide12.svg', './assets/slides/slide13.svg', './assets/slides/slide14.svg', './assets/slides/slide15.svg', './assets/slides/slide16.svg', './assets/slides/slide18.svg', './assets/slides/slide17.svg', './assets/slides/slide19.svg', './assets/slides/slide20.svg', './assets/slides/slide21.svg', './assets/slides/slide22.svg', './assets/slides/slide23.svg' ]
+  slides: string [] = ['./assets/slides/slide1.svg', './assets/slides/slide2.svg', './assets/slides/slide3.svg', './assets/slides/slide4.svg', './assets/slides/slide5.svg', './assets/slides/slide6.svg', './assets/slides/slide7.svg', './assets/slides/slide8.svg', './assets/slides/slide9.svg', './assets/slides/slide10.svg', './assets/slides/slide11.svg', './assets/slides/slide12.svg', './assets/slides/slide13.svg', './assets/slides/slide16.svg', './assets/slides/slide14.svg', './assets/slides/slide15.svg', './assets/slides/slide18.svg', './assets/slides/slide17.svg', './assets/slides/slide19.svg', './assets/slides/slide20.svg', './assets/slides/slide21.svg', './assets/slides/slide22.svg', './assets/slides/slide23.svg' ]
   i=0;
 
   getSlide() {
